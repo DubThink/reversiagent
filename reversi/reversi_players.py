3aab93ee64c30b408b2eda094615bfa764ed4df9
kerrybuckman <kbuckman@ithaca.edu>
2019-10-20 00:08:37 -0400

    new minimax player implementation

--- reversi/reversi_players.py
+++ reversi/reversi_players.py
@@ -185,3 +185,94 @@ def minimax(board, move, max_symbol, is_max=False, depth=0):
         return max(vals)
     else:
         return min(vals)
+
+
+
+"""
+Minimax player implementation
+@author Kerry Buckman
+"""
+class MinimaxPlayer:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+        valid_moves = board.calc_valid_moves(self.symbol) #all valid moves
+        max_node = {} #dictionary of moves to their values
+
+        #for each move, call minimax and get the evaluation
+        #store in dictionary max node (key is move, value is value)
+        for i in range(len(valid_moves)):
+            board2 = copy.deepcopy(board)
+            board2.make_move(self.symbol, valid_moves[i])
+            move_val = self.minimax(board2, 3, 1, False)
+            max_node[tuple(valid_moves[i])] = move_val
+
+
+        #find the node with the highest max val, return it
+        max_val = max_node.get(tuple(valid_moves[0]))
+        max_val_key = tuple(valid_moves[0]) # the key that matches with the highest value
+        for x in max_node:
+            if max_node.get(x) > max_val:
+                max_val = max_node.get(x)
+                max_val_key = x
+        return max_val_key
+
+    # returns value of a node (move)
+
+    def minimax(self, board, max_depth, current_depth, my_turn):
+
+        if my_turn == True:
+            move_list = board.calc_valid_moves(self.symbol)
+
+            if board.game_continues() == False:
+                return self.eval_board(board)
+
+            if len(move_list) == 0:  # end of tree or invalid move
+                return self.minimax(board, max_depth, current_depth+1, False)
+
+            if current_depth == max_depth:  # deep as can go
+                return self.eval_board(board)
+
+            values = set()
+            for i in range(len(move_list)):
+                board2 = copy.deepcopy(board)
+                board2.make_move(self.symbol, move_list[i])
+                val = self.minimax(board2, max_depth, current_depth + 1, False)
+                values.add(val)
+
+            return max(values)
+
+
+        else:
+            move_list = board.calc_valid_moves(board.get_opponent_symbol(self.symbol))
+
+            if board.game_continues() == False:
+                return self.eval_board(board)
+
+            if len(move_list) == 0:  # end of tree or invalid move
+                return self.minimax(board, max_depth, current_depth+1, True)
+
+            if current_depth == max_depth:    # deep as can go
+                return self.eval_board(board)
+
+            values = set()
+            for i in range(len(move_list)):
+                board2 = copy.deepcopy(board)
+                board2.make_move(board2.get_opponent_symbol(self.symbol), move_list[i])
+                val = self.minimax(board2, max_depth, current_depth + 1, True)
+                values.add(val)
+
+            return min(values)
+
+
+
+
+
+
+    #returns how many more pieces player 1 has than player 2
+    def eval_board(self, board):
+        scores = board.calc_scores()
+        if self.symbol == "X":
+            return scores.get("X")-scores.get("O")
+        return scores.get("O")-scores.get("X")
